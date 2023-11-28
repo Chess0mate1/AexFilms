a6662025a6a1ffdefdedf4fda0ab472cc29e27e4
Andrey Gomulko <andrey_gomulko@mail.ru>
2023-11-29 01:29:16 +0700

    #4 Add tests for repositories

---
+++ AexFilms.UnitTesting/DataAccess/Repositories/Reading/EfCollectionReadingRepositoryTestsBase.cs
@@ -0,0 +1,21 @@
+﻿using Chess0Mate1.Entity.Core;
+
+namespace AexFilms.UnitTesting.DataAccess.Repositories.Reading;
+
+public abstract class EfCollectionReadingRepositoryTestsBase<TEntity> : EfRepositoryTestsBase, IAsyncLifetime
+    where TEntity : EntityBase
+{
+    public async Task InitializeAsync()
+    {
+        await using var context = _factory.CreateDbContext();
+
+        var savedCollection = GetSavedCollection().Values.ToList();
+        await context.AddRangeAsync(savedCollection);
+
+        await context.SaveChangesAsync();
+    }
+
+    public Task DisposeAsync() => Task.CompletedTask;
+
+    protected abstract Dictionary<string, TEntity> GetSavedCollection();
+}
---
+++ AexFilms.UnitTesting/DataAccess/Repositories/Requesting/EfInitializationCheckingRepositoryTests.cs
@@ -0,0 +1,22 @@
+﻿using AexFilms.DataAccess.Repositories.Requesting;
+
+namespace AexFilms.UnitTesting.DataAccess.Repositories.Requesting;
+
+public class EfInitializationCheckingRepositoryTests : EfRepositoryTestsBase
+{
+    /// <remarks>
+    ///     see <see cref="EfInitializationCheckingRepository.IsNeeded"/> why always
+    /// </remarks>
+    [Fact]
+    public async Task IsNeeded_Always_ReturnsTrue()
+    {
+        // Arrange
+        var repository = new EfInitializationCheckingRepository(_factory);
+
+        // Act
+        var isNeeded = await repository.IsNeeded();
+
+        // Assert
+        Assert.True(isNeeded);
+    }
+}
