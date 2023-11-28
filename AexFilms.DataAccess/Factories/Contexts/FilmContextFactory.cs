fd59cab50357487cebab02ca328a2972259d0a83
Andrey Gomulko <andrey_gomulko@mail.ru>
2023-11-29 01:12:50 +0700

    #4 Add FilmContextFactory

---
+++ AexFilms.DataAccess/Factories/Contexts/FilmContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using AexFilms.DataAccess.Contexts;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace AexFilms.DataAccess.Factories.Contexts;
+
+/// <summary>
+///     Represents a factory for creating instances of the <see cref="FilmContext"/> database context.
+/// </summary>
+public class FilmContextFactory() : IDbContextFactory<FilmContext>
+{
+    /// <summary>
+    ///     Initialize the options for configuring the <see cref="FilmContext"/>.
+    /// </summary>
+    public required DbContextOptions<FilmContext> Options { private get; init; }
+
+    /// <summary>
+    ///     Creates and returns an instance of the <see cref="FilmContext"/> database context.
+    /// </summary>
+    /// <returns>An instance of the <see cref="FilmContext"/> class.</returns>
+    public FilmContext CreateDbContext() => new(Options);
+}
