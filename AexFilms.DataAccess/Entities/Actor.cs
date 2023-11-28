0241fb09c9db6b599bdba1663e6804aa0336bc8d
Andrey Gomulko <andrey_gomulko@mail.ru>
2023-11-28 20:40:04 +0700

    #4 Add entities: actor, genre, film

---
+++ AexFilms.DataAccess/Entities/Actor.cs
@@ -0,0 +1,20 @@
+﻿using Chess0Mate1.Entity.Core;
+
+namespace AexFilms.DataAccess.Entities;
+
+
+/// <summary>
+///     Represents an entity for an actor.
+/// </summary>
+public class Actor : EntityBase
+{
+    /// <summary>
+    ///     Gets or sets the full name of the actor/>
+    /// </summary>
+    public required string FullName { get; set; };
+
+    /// <summary>
+    ///     Gets or initializes the collection of films associated with the actor with the passed or default value.
+    /// </summary>
+    public ICollection<Film> FilmCollection { get; init; } = new List<Film>();
+}
---
+++ AexFilms.DataAccess/Entities/Genre.cs
@@ -0,0 +1,19 @@
+﻿using Chess0Mate1.Entity.Core;
+
+namespace AexFilms.DataAccess.Entities;
+
+/// <summary>
+///     Represents an entity for a film's genre.
+/// </summary>
+public class Genre : EntityBase
+{
+    /// <summary>
+    ///     Gets or sets the name of the genre.
+    /// </summary>
+    public required string Name { get; set; }
+
+    /// <summary>
+    ///     Gets or initializes the collection of films associated with the genre.
+    /// </summary>
+    public ICollection<Film> FilmCollection { get; init; } = new List<Film>();
+}
