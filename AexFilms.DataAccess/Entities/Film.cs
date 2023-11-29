7d01239e3cf473d5b69cbd0d93fdc2ef1f7c746c
Andrey Gomulko <andrey_gomulko@mail.ru>
2023-11-30 02:43:30 +0700

    #5 add properties in lowercase to ensure compatibility with sqlite

--- AexFilms.DataAccess/Entities/Film.cs
+++ AexFilms.DataAccess/Entities/Film.cs
@@ -7,10 +7,30 @@ namespace AexFilms.DataAccess.Entities;
 /// </summary>
 public class Film : EntityBase
 {
+    private string _title = null!;
+
     /// <summary>
-    ///     Gets or sets the title of the film
+    ///     Gets or sets the title of the film and updates <see cref="LowerCaseTitle"/>
     /// </summary>
-    public required string Title { get; set; }
+    public required string Title
+    {
+        get => _title;
+        set
+        {
+            _title = value;
+            LowerCaseTitle = value.ToLower();
+        }
+    }
+
+    /// <summary>
+    ///     Gets the lowercased version of the <see cref="Title"/>, used for case-insensitive comparisons.
+    /// </summary>
+    /// <remarks>
+    ///     *<see href="https://ru.stackoverflow.com/a/403136">
+    ///         since there is poor unicode support in sqlite, an additional field is used.
+    ///     </see>
+    /// </remarks>
+    public string LowerCaseTitle { get; private set; } = null!;
 
     /// <summary>
     ///     Gets or sets the binary image data associated with the film
