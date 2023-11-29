ddb0287c23f1c752ef79cd3023378b1bda5033d9
Andrey Gomulko <andrey_gomulko@mail.ru>
2023-11-30 02:51:50 +0700

    #5 create messages about changing and reset filter

---
+++ AexFilms.ViewModel/Messages/FilterUpdatedMessage.cs
@@ -0,0 +1,11 @@
+﻿using AexFilms.ViewModel.Filters;
+
+using CommunityToolkit.Mvvm.Messaging.Messages;
+
+namespace AexFilms.ViewModel.Messages;
+
+/// <summary>
+///     Represents a message of an updated filter.
+/// </summary>
+/// <param name="value">The updated filter.</param>
+public class FilterUpdatedMessage(IFilter value) : ValueChangedMessage<IFilter>(value);
