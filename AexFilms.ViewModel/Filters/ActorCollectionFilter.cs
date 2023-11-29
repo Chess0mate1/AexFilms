1b60ab867a9e9c975508977048d7f3390fd5e5bd
Andrey Gomulko <andrey_gomulko@mail.ru>
2023-11-30 02:50:18 +0700

    #5  create filters for sending data in view models

---
+++ AexFilms.ViewModel/Filters/ActorCollectionFilter.cs
@@ -0,0 +1,22 @@
+﻿using AexFilms.DataAccess.Entities;
+
+namespace AexFilms.ViewModel.Filters;
+
+/// <summary>
+///     Represents a filter for selecting a collection of actors in the filtering process.
+/// </summary>
+public class ActorCollectionFilter : IFilter
+{
+    public string DisplayName { get; } = "Актёры";
+
+    public bool IsActive =>
+        Value.Any();
+
+    /// <summary>
+    ///     Gets or sets a value of filter
+    /// </summary>
+    public IEnumerable<Actor> Value { get; set; } = new List<Actor>();
+
+    public void Cancel() =>
+        Value = new List<Actor>();
+}
