74a71c4edd379cb01ddcf4423447dc8789ecc19e
Andrey Gomulko <andrey_gomulko@mail.ru>
2023-11-30 03:14:34 +0700

    #5 add stub filter

---
+++ AexFilms.UnitTesting/Stubs/StubFilter.cs
@@ -0,0 +1,20 @@
+﻿using AexFilms.ViewModel.Filters;
+
+namespace AexFilms.UnitTesting.Stubs;
+
+/// <summary>
+///     Represents a stub implementation of the <see cref="IFilter"/> interface for testing purposes.
+/// </summary>
+internal class StubFilter : IFilter
+{
+    public string DisplayName => "Подделка";
+    public bool IsActive { get; }
+
+    /// <summary>
+    ///     Gets or sets the value associated with the filter.
+    /// </summary>
+    public string Value { get; set; } = "";
+
+    public void Cancel() =>
+        Value = "";
+}
