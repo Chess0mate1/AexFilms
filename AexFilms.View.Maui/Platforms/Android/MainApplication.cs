8dd44cbad83e003858d0004cf7da6d84dc2c684f
Andrey Gomulko <andrey_gomulko@mail.ru>
2023-11-30 03:08:31 +0700

    #5 synchronize namespaces

--- AexFilms.View.Maui/Platforms/Android/MainApplication.cs
+++ AexFilms.View.Maui/Platforms/Android/MainApplication.cs
@@ -1,12 +1,12 @@
 ﻿using Android.App;
 using Android.Runtime;
 
-namespace AexFilms.View.Maui
+namespace AexFilms.View.Maui.Platforms.Android
 {
     [Application]
     public class MainApplication : MauiApplication
     {
-        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+        public MainApplication(nint handle, JniHandleOwnership ownership)
             : base(handle, ownership)
         {
         }
