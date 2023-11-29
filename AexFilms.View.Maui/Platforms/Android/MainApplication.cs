eb9d13fbcf12fee0895fd441e187a3ad3f8b0c71
Andrey Gomulko <andrey_gomulko@mail.ru>
2023-11-30 06:33:14 +0700

    #6 change namespace scope to file level

--- AexFilms.View.Maui/Platforms/Android/MainApplication.cs
+++ AexFilms.View.Maui/Platforms/Android/MainApplication.cs
@@ -1,16 +1,15 @@
 ﻿using Android.App;
 using Android.Runtime;
 
-namespace AexFilms.View.Maui.Platforms.Android
-{
-    [Application]
-    public class MainApplication : MauiApplication
-    {
-        public MainApplication(nint handle, JniHandleOwnership ownership)
-            : base(handle, ownership)
-        {
-        }
+namespace AexFilms.View.Maui.Platforms.Android;
 
-        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+[Application]
+public class MainApplication : MauiApplication
+{
+    public MainApplication(nint handle, JniHandleOwnership ownership)
+        : base(handle, ownership)
+    {
     }
+
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
--- AexFilms.View.Maui/Platforms/iOS/AppDelegate.cs
+++ AexFilms.View.Maui/Platforms/iOS/AppDelegate.cs
@@ -1,10 +1,9 @@
 ﻿using Foundation;
 
-namespace AexFilms.View.Maui.Platforms.iOS
+namespace AexFilms.View.Maui.Platforms.iOS;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
 {
-    [Register("AppDelegate")]
-    public class AppDelegate : MauiUIApplicationDelegate
-    {
-        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-    }
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
